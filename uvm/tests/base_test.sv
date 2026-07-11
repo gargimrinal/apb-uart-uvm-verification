0f2e58ac3d276929e3ee25f763ea69ef1cd94683
Gargi Mrinal <gargimrinal6@gmail.com>
2026-07-11 12:29:53 +0530

    Add files via upload

--- uvm/tests/base_test.sv
+++ uvm/tests/base_test.sv
@@ -22,41 +22,64 @@
 class base_test extends uvm_test;
 
     `uvm_component_utils(base_test)
-apb_env env;
+
+    apb_env env;
+
     function new(string name = "base_test",
                  uvm_component parent = null);
         super.new(name, parent);
     endfunction
 
- virtual function void build_phase(uvm_phase phase);
 
-    super.build_phase(phase);
+    virtual function void build_phase(uvm_phase phase);
+        super.build_phase(phase);
 
-    env = apb_env::type_id::create("env", this);
+        env = apb_env::type_id::create("env", this);
+    endfunction
 
-endfunction
-virtual function void end_of_elaboration_phase(uvm_phase phase);
-    super.end_of_elaboration_phase(phase);
 
-    uvm_top.print_topology();
-endfunction
-task run_phase(uvm_phase phase);
-apb_write_seq wr_seq;
-apb_read_seq  rd_seq;
+    virtual function void end_of_elaboration_phase(uvm_phase phase);
+        super.end_of_elaboration_phase(phase);
 
-phase.raise_objection(this);
+        uvm_top.print_topology();
+    endfunction
 
-wr_seq = apb_write_seq::type_id::create("wr_seq");
-rd_seq = apb_read_seq::type_id::create("rd_seq");
 
-wr_seq.start(env.agent.seqr);
+  virtual task run_phase(uvm_phase phase);
 
-#20ns;
+    apb_write_seq       wr_seq;
+    apb_read_seq        rd_seq;
+    apb_random_seq      rand_seq;
+    apb_write_read_seq  wr_rd_seq;
 
-rd_seq.start(env.agent.seqr);
+    phase.raise_objection(this);
 
-#100ns;
+    wr_seq    = apb_write_seq::type_id::create("wr_seq");
+    rd_seq    = apb_read_seq::type_id::create("rd_seq");
+    rand_seq  = apb_random_seq::type_id::create("rand_seq");
+    wr_rd_seq = apb_write_read_seq::type_id::create("wr_rd_seq");
 
-phase.drop_objection(this);
-    endtask
+    // Directed write
+    wr_seq.start(env.agent.seqr);
+
+    #20ns;
+
+    // Directed read
+    rd_seq.start(env.agent.seqr);
+
+    #20ns;
+
+    // Random transactions
+    rand_seq.start(env.agent.seqr);
+
+    #20ns;
+
+    // Write then immediate read
+    wr_rd_seq.start(env.agent.seqr);
+
+    #100ns;
+
+    phase.drop_objection(this);
+
+endtask
 endclass
