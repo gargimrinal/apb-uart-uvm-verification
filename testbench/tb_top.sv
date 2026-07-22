7515d7b7868b34cce45522fc576b8ec9ee26cc83
Gargi Mrinal <gargimrinal6@gmail.com>
2026-07-22 11:58:50 +0530

    Add files via upload

--- testbench/tb_top.sv
+++ testbench/tb_top.sv
@@ -40,6 +40,10 @@ module tb_top;
     //----------------------------------------------------
     apb_if  apb_vif(clk);
     uart_if uart_vif(clk);
+    
+    apb_assertions apb_assert_inst (
+    .apb_if(apb_vif)
+);
 
     //----------------------------------------------------
     // DUT
@@ -63,21 +67,40 @@ module tb_top;
         .tx_o      (uart_vif.tx),
         .event_o   (uart_vif.event_o)
     );
+//----------------------------------------------------
+// Monitor Connections
+//----------------------------------------------------
+assign uart_vif.baud_div       = {dut.regs_q[dut.DLM+'d8],
+                                  dut.regs_q[dut.DLL+'d8]};
+assign uart_vif.parity_enable = dut.regs_q[dut.LCR][3];
+assign uart_vif.stop_bits      = dut.regs_q[dut.LCR][2];
+assign uart_vif.data_bits      = dut.regs_q[dut.LCR][1:0];
 
-    //----------------------------------------------------
-    // Reset Generation
-    //----------------------------------------------------
-    initial begin
+assign uart_vif.parity_error   = dut.parity_error;
+assign uart_vif.rx_valid       = dut.rx_valid;
+assign uart_vif.rx = uart_vif.tx;
+   //----------------------------------------------------
+// Reset Generation
+//----------------------------------------------------
+initial begin
 
-        apb_vif.PRESETn = 0;
-        uart_vif.rst_n  = 0;
+    // Hold reset active
+    apb_vif.PRESETn = 0;
+    uart_vif.rst_n  = 0;
 
-        repeat(5) @(posedge clk);
+    // APB idle values
+    apb_vif.PSEL     = 0;
+    apb_vif.PENABLE  = 0;
+    apb_vif.PWRITE   = 0;
+    apb_vif.PADDR    = '0;
+    apb_vif.PWDATA   = '0;
 
-        apb_vif.PRESETn = 1;
-        uart_vif.rst_n  = 1;
+    repeat(5) @(posedge clk);
 
-    end
+    apb_vif.PRESETn = 1;
+    uart_vif.rst_n  = 1;
+
+end
 
     //----------------------------------------------------
     // UVM Configuration
